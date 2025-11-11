f188cf5cb1feb7aa39cb31c19e58d8530c8eb236
TheDarkKnight43 <james.nelsonfr@gmail.com>
2025-11-11 15:11:04 -0700

    added intake functions and slow drive

--- src/AutonFunction.cpp
+++ src/AutonFunction.cpp
@@ -68,5 +68,60 @@ void Curve(double DriveDistance, double TurnValue){
     DrivedesiredValue = (DriveDistance / distancePerRevolution) * 360; //This is in degrees
 
     waitUntil(TurnComplete == true && DriveComplete == true);
+    
 
+}
+void SDrive(double driveDistance){  //driveDistance is in centimeters
+
+    LeftDriveSmart.resetPosition();
+    wait(1, msec);
+    RightDriveSmart.resetPosition();
+    wait(1, msec);
+
+    TurndesiredValue = 0;
+
+    double distancePerRevolution = 23.2; // this is in cm, and it is the distance that the 2.75 in omni wheels go in one rotation
+
+    DrivedesiredValue = (driveDistance / distancePerRevolution) * 360; //This is in degrees
+
+    waitUntil(DriveComplete == true);  
+    wait(1, msec);
+}
+void intakestore(){
+        Intake1.setVelocity(100, percent);
+        Intake2.setVelocity(100, percent);
+        Intake3.setVelocity(100, percent);
+        Intake1.spin(forward);
+        Intake2.spin(reverse);
+        Intake3.spin(reverse);
+}
+void intakescoremiddle(){
+        Intake1.setVelocity(100, percent);
+        Intake2.setVelocity(100, percent);
+        Intake3.setVelocity(100, percent);
+        Intake1.spin(forward);
+        Intake2.spin(forward);
+        Intake3.spin(reverse);
+}
+void intakescoretop(){
+        Intake1.setVelocity(100, percent);
+        Intake2.setVelocity(100, percent);
+        Intake3.setVelocity(100, percent);
+        Intake1.spin(forward);
+        Intake2.spin(forward);
+        Intake3.spin(forward);
+}
+
+void intakeeject(){
+        Intake1.setVelocity(100, percent);
+        Intake2.setVelocity(100, percent);
+        Intake3.setVelocity(100, percent);
+        Intake1.spin(reverse);
+        Intake2.spin(reverse);
+        Intake3.spin(reverse);
+}
+void intakestop(){
+        Intake1.stop();
+        Intake2.stop();
+        Intake3.stop();
 }
