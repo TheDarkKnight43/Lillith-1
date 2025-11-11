1bc4af2b00105e5528bbf1052c07c720ad357b25
TheDarkKnight43 <james.nelsonfr@gmail.com>
2025-11-11 15:28:56 -0700

    added funcions

--- include/AutonFunction.h
+++ include/AutonFunction.h
@@ -1,3 +1,8 @@
 void Drive(double driveDistance);
 void Turn(double turnDegrees);
 void Curve(double driveDistance, double turnDegrees);
+void intakestore();
+void intakescoretop();
+void intakescoremiddle();
+void intakeeject();
+void SDrive();
